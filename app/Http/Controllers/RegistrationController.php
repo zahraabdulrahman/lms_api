4b7d5bc463d3c82508df824c4c5e47c4ab637510
zahra <alzahra@live.com>
2025-01-23 19:58:13 +0300

    feat: add resource controllers
    - CourseResource controller
    - StudentResource controller
    - API resource transformations
    - Relationship handling

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\RegistrationResource;
 
 class RegistrationController extends Controller
 {
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User; 
+use App\Http\Resources\StudentResource;
 
 class UserController extends Controller
 {
